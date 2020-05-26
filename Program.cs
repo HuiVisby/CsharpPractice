f96d0bd7b1347ed011725a4c020611896ed1edc7
HuiVisby <yanghui327@outlook.com>
2020-05-26 18:45:16 +0200

    first commit of csharp

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practice
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Mata in tal 1:"); 
+         string inmata = Console.ReadLine();
+         int mitt
+         Console.WriteLine("Mata in tal 2:");
+         string inmataa = Console.ReadLine();
+         string fras = "Du heter:" + efternamn + "," + fornamn;
+         Console.WriteLine(fras);
+         
+      }
+   }
+}
