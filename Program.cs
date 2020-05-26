3edb609dca7e3682dc1db1098b0878b1a5bd90b0
HuiVisby <yanghui327@outlook.com>
2020-05-26 19:03:08 +0200

    open RD

--- Program.cs
+++ Program.cs
@@ -10,12 +10,7 @@ namespace practice
       static void Main(string[] args)
       {
          Console.WriteLine("Mata in tal 1:"); 
-         string inmata = Console.ReadLine();
-         int mitt
-         Console.WriteLine("Mata in tal 2:");
-         string inmataa = Console.ReadLine();
-         string fras = "Du heter:" + efternamn + "," + fornamn;
-         Console.WriteLine(fras);
+
          
       }
    }
